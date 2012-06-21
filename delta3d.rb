7957ff0d76b79b77aa404d3e2c7b55072dd3fea6
Joris van Zundert <joris.van.zundert@gmail.com>
2012-06-22 01:58:28 +0200

    Stab at decent modeling

--- delta3d.rb
+++ delta3d.rb
@@ -6,7 +6,8 @@ class Delta3D
   def initialize( json_string )
     @token_size = 0
     @max_position = 0
-    source = JSON.parse(json_string, {:max_nesting => 7} )
+
+    source = JSON.parse(json_string, {:max_nesting => 8} )
 
     # Valid structures
     # { "lines": [ { "id": line_id, "positions": [ { "id": position_id, "token": { "surface": surface, "lemmas": [ lemma1, lemma2, ... ] } }, ... ] }, ... ] }
@@ -23,17 +24,16 @@ class Delta3D
     first_key = source.keys.first 
     self.send( first_key, source[ first_key ] ) if ["lines", "positions"].include? first_key
   end
-  
-  def lines( lines_unordered )
-    @lines_ordered = lines_unordered.sort do |line_a,line_b|
-      guard_invalid if !line_a.is_a? Hash or !line_b.is_a? Hash
-      guard_invalid if (line_a["id"].nil? or line_b["id"].nil?)
-      Integer( line_a["id"] ) <=> Integer( line_b["id"] ) 
-    end
+    
+  def lines( lines_source )
+    lines_unordered = convert( lines_source )
+    @lines_ordered = lines_unordered.sort{ |line_a,line_b| line_a.line_id <=> line_b.line_id }
   end
 
-  def guard_invalid
-    raise InvalidJSONStructureException, "JSON input has not the right structure" 
+  def convert( lines_source )
+    lines = Array.new
+    lines_source.each { |line| lines.push( Line.new(line) ) }
+    lines
   end
   
   def lines_ordered
@@ -41,50 +41,163 @@ class Delta3D
   end
   
   def token_size
-    lines_ordered.each { |line| @token_size += line["positions"].size } if @token_size == 0
+    lines_ordered.each { |line| @token_size += line.tokens.size } if @token_size == 0
     @token_size
   end
     
   def max_position
     if @max_position == 0
       lines_ordered.each do |line| 
-        line["positions"].each do |position|
-          @max_position = Integer(position["id"]) if Integer(position["id"]) > @max_position
+        line.tokens.each do |token|
+          @max_position = token.position if token.position > @max_position
         end
       end
     end
     @max_position
   end
-  
-  def create_background(n)
-    quotient = ( max_position.to_f / n.to_f ).ceil
-    lemmas_in_quotients = Array.new(n,0)
-    background = lines_ordered.inject(Hash.new) do |base, line| 
-      line["positions"].each do |position_token|
-        position_token["token"]["lemmas"].each do |lemma|
-          base[lemma] = Array.new(n,0) if !base.include?( lemma )
-          position = position_token["id"]
-          index = (((Integer(position))-1)/quotient).floor
-          if !(index>=n)
-            base[lemma][index] += 1 
-            lemmas_in_quotients[index] += 1
-          else
-            puts "#{position}: #{index} (#{token_size}, #{n})"
+
+end
+
+
+class Background < Hash
+end
+
+class BackgroundFactory
+
+  def self.create( ordered_lines, parts )
+    frequencies = Hash.new
+    lemma_counts = Array.new(parts,0)
+    part_size = ( ordered_lines.size.to_f / parts.to_f ).ceil
+    parts.times do |part|
+      ordered_lines[(part*part_size),part_size].each do |line| 
+        line.tokens.each do |token|
+          token.lemmas.each do |lemma|
+            frequencies[lemma] = Array.new(parts,0) if !frequencies.include?( lemma )
+            frequencies[lemma][part] += 1
+            lemma_counts[part] += 1
           end
         end
       end
-      base
     end
-    background.each do |lemma, frequencies|
-      frequencies.each_index { |index| frequencies[index] = frequencies[index].to_f/lemmas_in_quotients[index].to_f}
-      mean = ( frequencies.inject(0) { |sum,f| sum + f } ) / n
-      stdev = Math.sqrt( ( frequencies.inject(0) { |stdevsum,f| stdevsum +  (f-mean)**2 } ) / n )
-      background[lemma] = [mean, stdev]
+    background = Background.new
+    frequencies.each do |lemma, frequencies|
+      frequencies.each_index { |part| frequencies[part] = frequencies[part].to_f/lemma_counts[part].to_f}
+      mean = ( frequencies.inject(0) { |sum,f| sum + f } ) / parts
+      stdev = Math.sqrt( ( frequencies.inject(0) { |stdevsum,f| stdevsum +  (f-mean)**2 } ) / parts )
+      background[lemma] = [mean,stdev]
     end
-  background
+    background
+  end
+
+end
+
+
+class Window
+  
+  def initialize( ordered_lines, window_size )
+    @lemma_count = 0
+    @frequencies = Hash.new
+    @window_size = window_size
+    @lines = ordered_lines
+    @lines[0, @window_size].each do |line|
+      add_frequencies( line )
+    end
+  end
+  
+  def frequencies
+    @frequencies
+  end
+  
+  def lemma_count
+    @lemma_count
+  end
+  
+  def add_frequencies( line )
+    line.tokens.each do |token|
+      token.lemmas.each do |lemma|
+        !frequencies.include?( lemma ) ? frequencies[lemma] = 1 : frequencies[lemma] += 1
+        @lemma_count += 1
+      end
+    end
+  end
+
+  def remove_frequencies( line )
+    line.tokens.each do |token|
+      token.lemmas.each do |lemma|
+        frequencies[lemma] -= 1
+        frequencies.delete(lemma) if frequencies[lemma] == 0
+        @lemma_count -= 1
+      end
+    end
+  end
+  
+  def each(&block)
+    @lines[@window_size, @lines.length].each_with_index do |line,index|
+      yield self
+      add_frequencies( line )
+      remove_frequencies( @lines[index] )
+    end
+    yield self
   end
   
 end
+
+
+class Line
+  
+  def initialize( line_source )
+    guard_invalid if !line_source.is_a? Hash
+    guard_invalid if line_source["id"].nil?
+    guard_invalid if line_source["positions"].nil?
+    @line_id = Integer( line_source["id"] )
+    @tokens = line_source["positions"].collect { |position| Token.new( position ) }
+  end
+
+  def guard_invalid
+    raise InvalidJSONStructureException, "JSON input has not the right structure" 
+  end
+  
+  def line_id
+    @line_id
+  end
+  
+  def tokens
+    @tokens
+  end
+  
+end
+
+
+class Token
+
+  def initialize( token_source )
+    guard_invalid if !token_source.is_a? Hash
+    guard_invalid if token_source.size > 2
+    guard_invalid if token_source["id"].nil? or token_source["token"].nil? 
+    guard_invalid if token_source["token"]["surface"].nil?
+    @position = Integer( token_source["id"] )
+    @surface = token_source["token"]["surface"]
+    @lemmas = token_source["token"]["lemmas"]
+  end
+
+  def guard_invalid
+    raise InvalidJSONStructureException, "JSON input has not the right structure" 
+  end
+
+  def position
+    @position
+  end
+  
+  def surface
+    @surface
+  end
+  
+  def lemmas
+    @lemmas
+  end
+  
+end
+
 
 class InvalidJSONStructureException < StandardError
 end
--- delta3d_test.rb
+++ delta3d_test.rb
@@ -4,17 +4,22 @@ require 'test/unit'
 require 'json/pure'
 
 class Delta3DTest < Test::Unit::TestCase
-    
+   
+  def mock_positions
+    token_array = Array.new
+    token_array.push( { "id" => "102", "token" => { "surface" => "aword", "lemmas" => [ "lemma1", "lemma2" ] } } )
+    token_array.push( { "id" => "101", "token" => { "surface" => "aword2", "lemmas" => [ "lemma3", "lemma4" ] } } )
+    token_array.push( { "id" => "103", "token" => { "surface" => "aword3", "lemmas" => [ "lemma5", "lemma6" ] } } )
+  end
+  
   def test_not_numbers_as_key
     lines_arr = Array.new
-    lines_arr.push( { "id" => "3", "positions" => "whatever" } )
-    lines_arr.push( { "id" => "foo", "positions" => "whatever2" } )
-    lines_arr.push( { "id" => "2", "positions" => "whatever3" } )
+    lines_arr.push( { "id" => "3", "positions" => mock_positions } )
+    lines_arr.push( { "id" => "foo", "positions" => mock_positions } )
+    lines_arr.push( { "id" => "2", "positions" => mock_positions } )
     text_hash = { "lines" => lines_arr }
-    begin 
+    assert_raise( ArgumentError ) do
       delta3d = Delta3D.new( JSON.generate( text_hash ) )
-    rescue Exception => excp
-      assert( excp.message.index('invalid value for Integer: "foo"') != nil )
     end
   end
   
@@ -28,33 +33,103 @@ class Delta3DTest < Test::Unit::TestCase
   def test_lines_ordering
     #  "lines" => [ { id => "1", "positions" => [ { id => "100", token => { "surface" => "aword", "lemmas" => [ "lemma1", "lemma2" ] } } ] } ] }
     lines_arr = Array.new
-    lines_arr.push( { "id" => "3", "positions" => "whatever" } )
-    lines_arr.push( { "id" => "1", "positions" => "whatever2" } )
-    lines_arr.push( { "id" => "2", "positions" => "whatever3" } )
+    lines_arr.push( { "id" => "3", "positions" => mock_positions } )
+    lines_arr.push( { "id" => "1", "positions" => mock_positions } )
+    lines_arr.push( { "id" => "2", "positions" => mock_positions } )
+    # This kind of screams for duplicate detection, no?
+    # Duplicates (of positions for instance) are structurally not wrong, but conceptually odd.
+    # In practice last in will overwrite other values
     text_hash = { "lines" => lines_arr }
     delta3d = Delta3D.new( JSON.generate( text_hash ) )
-    id = "0"
+    id = 0
     delta3d.lines_ordered.each do |line| 
-      assert( Integer( line["id"] ) == (Integer( id ) + 1)  ) 
-      id = line["id"]
+      assert( line.line_id == id+1  ) 
+      id = line.line_id
+    end
+  end
+
+  def test_token_id_not_number
+    token_hash = { "id" => "foo", "token" => { "surface" => "aword", "lemmas" => [ "lemma1", "lemma2" ] } }
+    assert_raise( ArgumentError ) do
+      delta3d = Token.new(token_hash)
     end
   end
   
-  # def test_real_life_ordering
-  #   text_json = File.read( 'fixtures/full_hash_excerpt_converted.json' )
-  #   delta3d = Delta3D.new( text_json )
-  #   id = "0"
-  #   delta3d.lines_ordered.each do |line| 
-  #     assert( Integer( line["id"] ) == (Integer( id ) + 1)  ) 
-  #     id = line["id"]
-  #   end
-  # end
+  def test_token_hash_wrong_size
+    token_hash = { "id" => "100", "token" => { "surface" => "aword", "lemmas" => [ "lemma1", "lemma2" ] }, "wrong" => "error" }
+    assert_raise( InvalidJSONStructureException ) do 
+      token = Token.new(token_hash)
+    end
+  end
 
+  def test_token_not_hash
+    token_hash = [ "id", "100", "token", "surface", "aword", "lemmas" ]
+    assert_raise( InvalidJSONStructureException ) do 
+      token = Token.new(token_hash)
+    end
+  end
+
+  def test_token_hash_invalid
+    invalid_token_hashes = Array.new
+    invalid_token_hashes.push( { "noid" => "100", "token" => { "surface" => "aword", "lemmas" => [ "lemma1", "lemma2" ] } } )
+    invalid_token_hashes.push( { "id" => "100", "notoken" => { "surface" => "aword", "lemmas" => [ "lemma1", "lemma2" ] } } )
+    invalid_token_hashes.push( { "id" => "100", "token" => { "nosurface" => "aword", "lemmas" => [ "lemma1", "lemma2" ] } } )
+    invalid_token_hashes.each do |position| 
+      assert_raise( InvalidJSONStructureException ) do 
+        token = Token.new(position)
+      end
+    end
+  end
+  
+  def test_no_lemmas_in_token_okay
+    token_hash = { "id" => "100", "token" => { "surface" => "aword" } }
+    token = Token.new(token_hash)
+    assert_equal( Integer( token_hash["id"] ), token.position )
+    assert_equal( token_hash["token"]["surface"], token.surface )
+    assert( token.lemmas.nil? )
+  end
+
+  def test_token
+    token_array = Array.new
+    positions_hash = { "positions" => token_array }
+    token_array.push( { "id" => "100", "token" => { "surface" => "aword", "lemmas" => [ "lemma1", "lemma2" ] } } )
+    token_array.push( { "id" => "120", "token" => { "surface" => "aword2", "lemmas" => [ "lemma3", "lemma4" ] } } )
+    token_array.push( { "id" => "103", "token" => { "surface" => "aword3", "lemmas" => [ "lemma5", "lemma6" ] } } )
+    positions_hash["positions"].each do |position|
+      token = Token.new(position)
+      assert_equal( Integer( position["id"] ), token.position )
+      assert_equal( position["token"]["surface"], token.surface )
+      token.lemmas.each{ |lemma| assert( position["token"]["lemmas"].include? lemma ) }
+    end
+  end
+    
+  def test_real_life_ordering
+    text_json = File.read( 'fixtures/full_hash_excerpt_converted.json' )
+    delta3d = Delta3D.new( text_json )
+    id = 0
+    delta3d.lines_ordered.each do |line| 
+      assert( line.line_id > id )
+      id = line.line_id
+    end
+  end
+  
   def test_lines_structure_fails_id
     #  "lines" => [ { id => "1", "positions" => [ { id => "100", token => { "surface" => "aword", "lemmas" => [ "lemma1", "lemma2" ] } } ] } ] }
     lines_arr = Array.new
     lines_arr.push( { "id" => "3", "positions" => "whatever" } )
     lines_arr.push( { "not_an_id" => "1", "positions" => "whatever2" } )
+    lines_arr.push( { "id" => "2", "positions" => "whatever3" } )
+    text_hash = { "lines" => lines_arr }
+    assert_raise( InvalidJSONStructureException ) do 
+      delta3d = Delta3D.new( JSON.generate( text_hash ) )
+    end
+  end
+
+  def test_lines_structure_fails_positions
+    #  "lines" => [ { id => "1", "positions" => [ { id => "100", token => { "surface" => "aword", "lemmas" => [ "lemma1", "lemma2" ] } } ] } ] }
+    lines_arr = Array.new
+    lines_arr.push( { "id" => "3", "NO_positions_HERE" => "whatever" } )
+    lines_arr.push( { "id" => "1", "positions" => "whatever2" } )
     lines_arr.push( { "id" => "2", "positions" => "whatever3" } )
     text_hash = { "lines" => lines_arr }
     assert_raise( InvalidJSONStructureException ) do 
@@ -74,17 +149,88 @@ class Delta3DTest < Test::Unit::TestCase
     end
   end
   
-  # def test_large
-  #   text_json = File.read( 'fixtures/full_hash_converted.json' )
-  #   delta3d = Delta3D.new( text_json )
-  #   assert_equal( 63089, delta3d.token_size )
-  #   assert_equal( 74301, delta3d.max_position )
-  # end
-  
-  def test_background
-    text_json = File.read( 'fixtures/full_hash_excerpt_converted.json' )
+  def test_real_life_size
+    text_json = File.read( 'fixtures/full_hash_converted.json' )
     delta3d = Delta3D.new( text_json )
-    puts delta3d.create_background(2).inspect
+    assert_equal( 63089, delta3d.token_size )
+    assert_equal( 74301, delta3d.max_position )
+    assert_nothing_raised do
+      BackgroundFactory.create(delta3d.lines_ordered,10)
+    end
+  end
+  
+  def mock_lines_source
+    random_vocabular = ["a", "word", "seldom", "travels", "alone", "its", "companions", "are", "characters"]
+    random_lemmas = [ "lem1", "lem2", "lem3", "lem4", "lem5", "lem6" ]
+    lines_source = Array.new
+    line_count = 10
+    position_count = 1
+    line_count.times do |line_number|
+      position_array = Array.new
+      token_count = 3 + rand(3)
+      token_count.times do 
+        lemma_array = Array.new
+        (1 + rand(2)).times do
+          lemma_array.push( random_lemmas[rand(random_lemmas.length)] )
+        end
+        surface = random_vocabular[rand(random_vocabular.length)]
+        token_hash = { "surface" => surface, "lemmas" => lemma_array }
+        position_array.push( { "id" => "#{position_count}", "token" => token_hash } )
+        position_count =+ 1
+      end
+      lines_source.push( { "id" => "#{line_number}", "positions" => position_array } )
+    end
+    lines_source
+  end
+  
+  def test_window
+    lines_source = mock_lines_source
+    test_windows = []
+    8.times do |line_number|
+      test_window = { "lines" => lines_source[line_number,3], "lemma_count" => 0, "frequencies" => {} }
+      test_window["lines"].inject(test_window) do |test_window,line| 
+        line["positions"].inject(test_window) do |test_window,position|
+          position["token"]["lemmas"].inject(test_window) do |test_window,lemma|
+            test_window["lemma_count"] += 1
+            test_window["frequencies"].include?(lemma) ? test_window["frequencies"][lemma] += 1 : test_window["frequencies"][lemma] = 1
+            test_window
+          end
+        end
+      end
+      test_windows.push( test_window )
+    end
+    lines_arr = lines_source.collect { |line| Line.new(line) }
+    window_size = 3 # with 10 lines, we expect 8 windows of 3 lines
+    window = Window.new( lines_arr, window_size )
+    window_count = 0
+    window.each do |window|
+      #puts test_windows[window_count]["lines"].inspect
+      assert_equal test_windows[window_count]["lemma_count"], window.lemma_count
+      assert_equal test_windows[window_count]["frequencies"], window.frequencies
+      window_count += 1
+    end
+    assert_equal 8, window_count 
+  end
+
+  def test_background
+    lines_source = mock_lines_source
+    lines_arr = lines_source.collect { |line| Line.new(line) }
+    parts = 3
+    test_stats = Array.new(3) {[0,0]} #3 parts, frequency, total lemmas in part
+    partsize = 4
+    parts.times do |part|
+      lines_arr[(part*partsize),partsize].each do |line|
+        line.tokens.each do |token|
+          token.lemmas.each do |lemma|
+            test_stats[part][0] += 1 if lemma.eql?( "lem6" )
+            test_stats[part][1] += 1
+          end
+        end
+      end
+    end
+    mean = ( test_stats.collect{ |stat| stat[0].to_f/stat[1].to_f }.inject(0) { |sum,f| sum + f } ) / parts
+    background = BackgroundFactory.create(lines_arr,3)
+    assert_equal mean, background["lem6"][0]
   end
 
 end
